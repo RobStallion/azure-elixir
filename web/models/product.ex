2f415777c09aa0ac92d537b4289252df1692176d
robstallion <robertefrancis18@gmail.com>
2017-06-19 15:28:21 +0100

    make product_id the primary key

--- web/models/product.ex
+++ web/models/product.ex
@@ -1,8 +1,8 @@
 defmodule Test.Product do
   use Test.Web, :model
 
+  @primary_key {:ProductId, :integer, autogenerate: false}
   schema "Products" do
-    field :ProductId, :integer
     field :ProductName, :string
   end
 end

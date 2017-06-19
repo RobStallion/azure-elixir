3c8345930740422771bb3e9dec7dd4fded08d178
robstallion <robertefrancis18@gmail.com>
2017-06-19 15:21:29 +0100

    add mssqlex to application list

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Test.Mixfile do
   def application do
     [mod: {Test, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :mssqlex]]
   end
 
   # Specifies which paths to compile per environment.
